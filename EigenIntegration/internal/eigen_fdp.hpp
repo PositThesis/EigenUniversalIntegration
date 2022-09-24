964b113b678d75211aecd4ce3fff9945db5aaacf
Yannik Böttcher <yannikboettcher@outlook.de>
2022-09-24 22:31:30 +0200

    make sparse product non reliant on nonZeros()

--- EigenIntegration/internal/eigen_fdp.hpp
+++ EigenIntegration/internal/eigen_fdp.hpp
@@ -33,17 +33,15 @@ typename MultiplyReturnType<Lhs, Rhs>::type eigen_fdp(const Lhs &lhs, const Rhs 
   assert(rhs.rows() == 1 || rhs.cols() == 1);
   assert(lhs.rows() * lhs.cols() == rhs.rows() * rhs.cols());
 
-  // first, turn potentially sparse vectors into dense vectors by extracting the non-zero elements
+  // first, turn sparse vectors into dense vectors by extracting the non-zero elements
   if constexpr (is_sparse<Lhs> && is_sparse<Rhs>) {
-    Eigen::Index max_size = lhs.size() > rhs.size() ? lhs.size() : rhs.size();
-
-    Eigen::VectorX<typename Lhs::Scalar> lhs_dense = Eigen::VectorX<typename Lhs::Scalar>::Zero(max_size);
-    Eigen::VectorX<typename Rhs::Scalar> rhs_dense = Eigen::VectorX<typename Rhs::Scalar>::Zero(max_size);
+    Eigen::VectorX<typename Lhs::Scalar> lhs_dense = Eigen::VectorX<typename Lhs::Scalar>::Zero(depth);
+    Eigen::VectorX<typename Rhs::Scalar> rhs_dense = Eigen::VectorX<typename Rhs::Scalar>::Zero(depth);
 
     // about that 0: according to some simple tests, it should always work out, given our premise that one of the axes has to have unit size
+    Eigen::Index current_index = 0;
     typename Eigen::InnerIterator<Lhs> lit(lhs, 0);
     typename Eigen::InnerIterator<Rhs> rit(rhs, 0);
-    Eigen::Index current_index = 0;
     while(lit && rit) {
       if (lit.index() < rit.index()) {
         ++lit;
@@ -72,29 +70,33 @@ typename MultiplyReturnType<Lhs, Rhs>::type eigen_fdp(const Lhs &lhs, const Rhs 
     return eigen_fdp(lhs_dense, rhs_dense);
 
   } else if constexpr (is_sparse<Lhs>) {
-    Eigen::VectorX<typename Lhs::Scalar> lhs_dense = Eigen::VectorX<typename Lhs::Scalar>::Zero(lhs.nonZeros());
-    Eigen::VectorX<typename Rhs::Scalar> rhs_dense = Eigen::VectorX<typename Rhs::Scalar>::Zero(lhs.nonZeros());
+    Eigen::VectorX<typename Lhs::Scalar> lhs_dense = Eigen::VectorX<typename Lhs::Scalar>::Zero(depth);
+    Eigen::VectorX<typename Rhs::Scalar> rhs_dense = Eigen::VectorX<typename Rhs::Scalar>::Zero(depth);
 
-    typename Eigen::InnerIterator<Lhs> it(lhs, 0);
-    for(Eigen::Index idx = 0; idx < lhs.nonZeros(); idx++) {
-      lhs_dense(idx) = it.value();
-      rhs_dense(idx) = rhs(it.index());
-      ++it;
+    Eigen::Index current_index = 0;
+    for(typename Eigen::InnerIterator<Lhs> it(lhs, 0); it; ++it) {
+      lhs_dense(current_index) = it.value();
+      rhs_dense(current_index) = rhs(it.index());
+      current_index++;
     }
+    lhs_dense.conservativeResize(current_index);
+    rhs_dense.conservativeResize(current_index);
     #ifdef TEST_EIGEN_USAGE
     eigen_usage_vector |= (uint64_t)EigenOverrideMask::SPARSE_LHS;
     #endif
     return eigen_fdp(lhs_dense, rhs_dense);
   } else if constexpr (is_sparse<Rhs>) {
-    Eigen::VectorX<typename Lhs::Scalar> lhs_dense = Eigen::VectorX<typename Lhs::Scalar>::Zero(rhs.nonZeros());
-    Eigen::VectorX<typename Rhs::Scalar> rhs_dense = Eigen::VectorX<typename Rhs::Scalar>::Zero(rhs.nonZeros());
+    Eigen::VectorX<typename Lhs::Scalar> lhs_dense = Eigen::VectorX<typename Lhs::Scalar>::Zero(depth);
+    Eigen::VectorX<typename Rhs::Scalar> rhs_dense = Eigen::VectorX<typename Rhs::Scalar>::Zero(depth);
 
-    typename Eigen::InnerIterator<Rhs> it(rhs, 0);
-    for(Eigen::Index idx = 0; idx < rhs.nonZeros(); idx++) {
-      lhs_dense(idx) = lhs(it.index());
-      rhs_dense(idx) = it.value();
-      ++it;
+    Eigen::Index current_index = 0;
+    for(typename Eigen::InnerIterator<Rhs> it(rhs, 0); it; ++it) {
+      lhs_dense(current_index) = lhs(it.index());
+      rhs_dense(current_index) = it.value();
+      current_index++;
     }
+    lhs_dense.conservativeResize(current_index);
+    rhs_dense.conservativeResize(current_index);
 
     #ifdef TEST_EIGEN_USAGE
     eigen_usage_vector |= (uint64_t)EigenOverrideMask::SPARSE_RHS;
@@ -109,11 +111,7 @@ typename MultiplyReturnType<Lhs, Rhs>::type eigen_fdp(const Lhs &lhs, const Rhs 
       constexpr size_t capacity = 20; // support vectors up to 1M elements
 
       if constexpr (!is_complex<typename Rhs::Scalar>) { // Lhs and Rhs real
-
         sw::universal::quire<nbits, es, capacity> q(0);
-
-        // decide which axes have to be iterated over. Each condition is evaluated
-        // only once per fdp.
         for (Eigen::Index idx = 0; idx < depth; idx++) {
           q += sw::universal::quire_mul(lhs.coeff(idx), rhs.coeff(idx));
         }
@@ -121,15 +119,11 @@ typename MultiplyReturnType<Lhs, Rhs>::type eigen_fdp(const Lhs &lhs, const Rhs 
         convert(q.to_value(), sum);
         return sum;
       } else { // Lhs real, rhs complex
-
         constexpr size_t nbits = Rhs::Scalar::value_type::nbits;
         constexpr size_t es = Rhs::Scalar::value_type::es;
         constexpr size_t capacity = 20; // support vectors up to 1M elements
         sw::universal::quire<nbits, es, capacity> q_real(0);
         sw::universal::quire<nbits, es, capacity> q_imag(0);
-
-        // decide which axes have to be iterated over. Each condition is evaluated
-        // only once per fdp.
         for (Eigen::Index idx = 0; idx < depth; idx++) {
           q_real +=
               sw::universal::quire_mul(lhs.coeff(idx), rhs.coeff(idx).real());
@@ -157,9 +151,6 @@ typename MultiplyReturnType<Lhs, Rhs>::type eigen_fdp(const Lhs &lhs, const Rhs 
       sw::universal::quire<nbits, es, capacity> q_imag(0);
 
       if constexpr (!is_complex<typename Rhs::Scalar>) { // Lhs complex, Rhs real
-
-        // decide which axes have to be iterated over. Each condition is evaluated
-        // only once per fdp.
         for (Eigen::Index idx = 0; idx < depth; idx++) {
           q_real +=
               sw::universal::quire_mul(lhs.coeff(idx).real(), rhs.coeff(idx));
@@ -178,10 +169,7 @@ typename MultiplyReturnType<Lhs, Rhs>::type eigen_fdp(const Lhs &lhs, const Rhs 
         convert(q_real.to_value(), sum_real);
         convert(q_imag.to_value(), sum_imag);
         return std::complex(sum_real, sum_imag);
-
       } else { // Lhs and Rhs complex
-        // decide which axes have to be iterated over. Each condition is evaluated
-        // only once per fdp.
         for (Eigen::Index idx = 0; idx < depth; idx++) {
           q_real +=
               sw::universal::quire_mul(lhs.coeff(idx).real(), rhs.coeff(idx).real());
